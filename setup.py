160c9f2b121eebef4419bf41dabce73a84759658
munwonjj <munwonjj@gmail.com>
2023-03-07 16:30:32 +0900

    modify setup

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    
+
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
